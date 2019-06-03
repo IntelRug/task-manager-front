da8f3ca8f92912e5e64233ccc0d69007cfa1e2d3
IntelRug <intelrug@outlook.com>
2019-06-03 21:09:22 +0400

    Add landing page to vue config

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,16 @@ const path = require('path');
 
 module.exports = {
   outputDir: path.resolve(__dirname, './build'),
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+    },
+    about: {
+      entry: 'src/landing.js',
+      template: 'public/landing.html',
+      filename: 'landing.html',
+    },
+  },
 };
